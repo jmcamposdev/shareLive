191f1a93412bbc3c9cacf7d04d66f39f7c645603
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-19 12:54:16 +0100

    Refactor MainMenu component to use dynamic menu links from useMenuLinks hook

---
+++ frontend/src/data/headerMenus.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    name: 'home',
+    link: '/'
+  },
+  {
+    name: 'about',
+    link: '/about'
+  },
+  {
+    name: 'contact',
+    link: '/contact'
+  },
+  {
+    name: 'rooms',
+    link: '/rooms'
+  }
+]
