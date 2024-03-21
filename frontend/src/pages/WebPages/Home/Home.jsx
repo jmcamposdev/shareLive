a973a2e777bb9e9fae02bd2965cafd18a5c83ac0
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-03-21 20:19:22 +0100

    Created two new components for the intro of the pages, one for white backgrounds and one for grey backrounds

--- frontend/src/pages/WebPages/Home/Home.jsx
+++ frontend/src/pages/WebPages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import HomeHero from '../../../components/Heros/HomeHero/HomeHero'
 import WebLayout from '../../../layout/WebLayout'
-import PageIntro from '../../../components/common/PageIntro/PageIntro'
 
 function Home () {
   return (
     <WebLayout>
       <HomeHero />
-      <PageIntro title='titulo' description='descripcion' backgroundImage='http://localhost:5174/src/assets/img/hero/home-hero.jpg' />
       <div style={{ height: '100vh' }} />
       <h1>Home</h1>
       <p>Welcome to the home page!</p>
