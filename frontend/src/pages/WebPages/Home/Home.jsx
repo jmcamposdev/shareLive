b844255468bb3853734fdcae82a1bff99137e80a
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-24 12:30:40 +0100

    Implement Why Choose Us section to home

--- frontend/src/pages/WebPages/Home/Home.jsx
+++ frontend/src/pages/WebPages/Home/Home.jsx
@@ -2,6 +2,7 @@ import HomeHero from '../../../components/Heros/HomeHero/HomeHero'
 import FunFact from '../../../components/Home/FunFact/FunFact'
 import PopularCities from '../../../components/Home/PopularCities/PopularCities'
 import Statistics from '../../../components/Home/Statistics/Statistics'
+import WhyChoose from '../../../components/Home/WhyChoose/WhyChoose'
 import RoomsNearYou from '../../../components/RoomsNearYou/RoomsNearYou'
 import WebLayout from '../../../layout/WebLayout'
 
@@ -13,6 +14,7 @@ function Home () {
       <Statistics />
       <PopularCities />
       <FunFact />
+      <WhyChoose />
     </WebLayout>
   )
 }
