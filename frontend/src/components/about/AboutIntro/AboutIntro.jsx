7a2e73b04132c63d24f9a6e0765fabd9d647961a
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-03-26 20:50:48 +0100

    Started changing the FaqIntro element to support dark mode, changed the blobs to better fit the color aswell

--- frontend/src/components/about/AboutIntro/AboutIntro.jsx
+++ frontend/src/components/about/AboutIntro/AboutIntro.jsx
@@ -2,15 +2,15 @@ import ThreeBlobs from '../../../assets/img/aboutUs/threeBlobs.png'
 
 const AboutIntro = () => {
   return (
-    <section className='about-us'>
+    <section className='about-us bg-f7f7f7 dark:bg-lightmidnight'>
       <div className='container mx-auto sm:px-4'>
         <div className='flex flex-wrap  mt80 mt0-md'>
 
           <div className='md:w-1/2 pr-4 pl-4 xl:mx-1/5'>
             <div className='about-box-1 wow fadeInLeft h-full flex flex-col justify-center' data-wow-delay='300ms' style={{ visibility: 'visible', animationDelay: '300ms', animationName: 'fadeInLeft' }}>
               <p className='text-xl mb-1.5 font-medium text-orangePrimary'>Our History</p>
-              <h2 className='title mb30'>Why Should you choose us</h2>
-              <p className='text mb20 fz15 text-2xl font-light'>
+              <h2 className='title mb30 dark:text-white'>Why Should you choose us</h2>
+              <p className='text mb20 fz15 text-2xl font-light dark:text-white'>
                 Choosing us means choosing <span className='font-semibold'>excellence</span> in student accommodation. With  over <span className='font-semibold'>two decades</span> of experience and a proven track record of providing  quality living spaces, we stand as a beacon of <span className='font-semibold'>reliability and comfort</span>  in the realm of student housing.
               </p>
             </div>
