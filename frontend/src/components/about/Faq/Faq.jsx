6f2cbeb24e71148e3f1f5a4b92c07bbc54c641e5
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-03-27 16:41:40 +0100

    Added the real questions and answers for the faq section

--- frontend/src/components/about/Faq/Faq.jsx
+++ frontend/src/components/about/Faq/Faq.jsx
@@ -2,29 +2,64 @@ import FaqAccordion from './FaqAccordion'
 
 const Faq = () => {
   return (
-    <section className='our-faq pb90 pt-0'>
+    <section className='our-faq pb90 pt-0 relative'>
+      <div className='absolute w-full h-full -z-[20] bg-f7f7f7 dark:bg-lightmidnight' />
       <div className='container mx-auto sm:px-4'>
         <div className='flex flex-wrap  wow fadeInUp' data-wow-delay='300ms'>
           <div className='w-full pr-4 pl-4'>
             <div className='ui-content'>
-              <h4 className='title'>Question About Selling</h4>
+              <h2 className='dark:text-white'>Frequently Asked Questions</h2>
+              <p className='dark:text-white mb-[60px]'>Home / Faq</p>
+              <h4 className='title dark:text-white'>Question About Selling</h4>
               <div className='accordion-style1 faq-page mb-4 lg:mb-12'>
                 <div className='accordion' id='accordionExample'>
-                  <FaqAccordion collapseNumber={1} question='Pregunta 1' answer='Respuesta 1' />
-                  <FaqAccordion collapseNumber={2} question='Pregunta 1' answer='Respuesta 1' />
-                  <FaqAccordion collapseNumber={3} question='Pregunta 1' answer='Respuesta 1' />
-                  <FaqAccordion collapseNumber={4} question='Pregunta 1' answer='Respuesta 1' />
+                  <FaqAccordion
+                    collapseNumber={1}
+                    question='How can I list my room for rent on your platform?'
+                    answer='Listing your room is simple! Just sign up as a host on our website,  provide details about your property and the available room, upload some  photos, and set your rental terms. Once approved, your listing will be  visible to potential renters.'
+                  />
+                  <FaqAccordion
+                    collapseNumber={2}
+                    question='What kind of rooms are typically available for rent on your platform?'
+                    answer='We offer a variety of room types, including single rooms, shared rooms,  and apartments with multiple bedrooms. Whether you are looking for a cozy  private space or a shared living arrangement, you will find options that  suit your needs.'
+                  />
+                  <FaqAccordion
+                    collapseNumber={3}
+                    question='How do I find roommates if I am interested in a shared living arrangement?'
+                    answer='If you are open to sharing a living space, you can indicate your  preference for roommates when searching for rooms. Additionally, our  platform allows you to connect with other users who are also seeking  roommates. You can chat with potential roommates, discuss preferences,  and mutually decide on a living arrangement that works for everyone.'
+                  />
+                  <FaqAccordion
+                    collapseNumber={4}
+                    question='Are the listings on your platform safe and reliable?'
+                    answer='We prioritize the safety and security of our users. All hosts and  listings undergo a verification process to ensure legitimacy. We also  encourage users to read reviews from previous renters and hosts to get a  sense of the experience. Additionally, our customer support team is  available to assist with any concerns or issues that may arise during  the rental process.'
+                  />
                 </div>
               </div>
             </div>
             <div className='ui-content'>
-              <h4 className='title'>Question About Renting</h4>
+              <h4 className='title dark:text-white'>Question About Rooms</h4>
               <div className='accordion-style1 faq-page mb-4 lg:mb-12'>
                 <div className='accordion' id='accordionExample2'>
-                  <FaqAccordion collapseNumber={5} question='Pregunta 1' answer='Respuesta 1' />
-                  <FaqAccordion collapseNumber={6} question='Pregunta 1' answer='Respuesta 1' />
-                  <FaqAccordion collapseNumber={7} question='Pregunta 1' answer='Respuesta 1' />
-                  <FaqAccordion collapseNumber={8} question='Pregunta 1' answer='Respuesta 1' />
+                  <FaqAccordion
+                    collapseNumber={5}
+                    question='Can I specify my preferences for the type of roommate I am looking for?'
+                    answer='Absolutely! When creating your profile or searching for rooms, you can  indicate your preferences for roommates based on factors such as gender,  age, lifestyle habits, and more. This helps ensure compatibility and a  harmonious living environment.'
+                  />
+                  <FaqAccordion
+                    collapseNumber={6}
+                    question=' What amenities are typically included in the rooms for rent?'
+                    answer='Amenities vary depending on the listing, but common inclusions are  furnishings (bed, desk, chair, etc.), access to shared spaces like the  kitchen and living room, utilities (electricity, water, internet), and  sometimes additional perks like laundry facilities or parking.'
+                  />
+                  <FaqAccordion
+                    collapseNumber={7}
+                    question='How does the rental process work?'
+                    answer='Once you find a room you are interested in, you can message the host  directly through our platform to inquire about availability and ask any  questions you may have. If you are ready to proceed, you can discuss  rental terms, sign any necessary agreements, and make payment  arrangements with the host.'
+                  />
+                  <FaqAccordion
+                    collapseNumber={8}
+                    question='What if I encounter issues during my stay in the rented room?'
+                    answer='We strive to provide a smooth rental experience, but if you encounter  any issues or concerns during your stay, you can reach out to our  customer support team for assistance. We are here to help resolve any  disputes between renters and hosts and ensure that everyone has a  positive experience on our platform.'
+                  />
                 </div>
               </div>
             </div>
--- frontend/src/components/about/Faq/FaqAccordion.jsx
+++ frontend/src/components/about/Faq/FaqAccordion.jsx
@@ -1,7 +1,7 @@
 const FaqAccordion = ({ collapseNumber, question, answer }) => {
   return (
-    <div className='accordion-item'>
-      <h2 className='accordion-header' id='headingThree'><button className='accordion-button ' type='button' data-bs-toggle='collapse' data-bs-target={`#collapse${collapseNumber}`} aria-expanded='true' aria-controls={`collapse${collapseNumber}`}>{question}</button></h2>
+    <div className='border dark:!border-lightmidnight dark:bg-midnight accordion-item dark:bg-lightmidnight'>
+      <h2 className=' accordion-header' id='headingThree'><button className='dark:text-white accordion-button ' type='button' data-bs-toggle='collapse' data-bs-target={`#collapse${collapseNumber}`} aria-expanded='true' aria-controls={`collapse${collapseNumber}`}>{question}</button></h2>
       <div id={`collapse${collapseNumber}`} className='accordion-collapse opacity-100 block collapse' aria-labelledby='headingThree' data-parent='#accordionExample2'>
         <div className='accordion-body'>
           <p>{answer}</p>
