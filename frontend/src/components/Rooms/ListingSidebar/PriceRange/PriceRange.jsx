6a8f68e5ee317dc6b8989f740931a2cfc8c1b17e
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-27 18:51:21 +0100

    Add filtering functionality to PriceRange component

--- frontend/src/components/Rooms/ListingSidebar/PriceRange/PriceRange.jsx
+++ frontend/src/components/Rooms/ListingSidebar/PriceRange/PriceRange.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import InputRange from 'react-input-range'
 import 'react-input-range/lib/css/index.css'
+import { RoomContext } from '../../../../context/roomContext'
 
 const PriceRange = () => {
+  const { filters, filterBy } = useContext(RoomContext)
+  const { min, max } = filters.price
   const [price, setPrice] = useState({ value: { min: 20, max: 70987 } })
 
   // price range handler
   const handleOnChange = (value) => {
     setPrice({ value })
+    filterBy.price(value.min, value.max)
   }
 
   return (
@@ -15,11 +19,11 @@ const PriceRange = () => {
       <div className='range-wrapper'>
         <InputRange
           formatLabel={() => ''}
-          maxValue={100000}
+          maxValue={1500}
           minValue={0}
+          value={{ min, max }}
           onChange={(value) => handleOnChange(value)}
           id='slider'
-          value={10}
         />
         <div className='d-flex align-items-center'>
           <span id='slider-range-value1'>${price.value.min}</span>
