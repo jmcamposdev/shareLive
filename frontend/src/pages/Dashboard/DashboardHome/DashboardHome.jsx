1b5cb105728e04b8424affbc3095ab369d3e3a48
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-28 15:56:04 +0100

    Add DashboardHome component to implement the dashboard home page

---
+++ frontend/src/pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from '../../../layout/DashboardLayout'
+
+function DashboardHome () {
+  return (
+    <DashboardLayout>
+      HOla
+    </DashboardLayout>
+  )
+}
+
+export default DashboardHome
