01d7acac1a2a476fec71ea91b2a0ec3870e7c1ce
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-28 15:55:58 +0100

    Add DashboardLayout component to implement the dashboard layout structure

---
+++ frontend/src/layout/DashboardLayout.jsx
@@ -0,0 +1,34 @@
+import SidebarDashboard from '../components/Dashboard/SidebarDashboard/SidebarDashboard'
+import DashboardFooter from '../components/Footer/DashboardFooter/DashboardFooter'
+import DashboardHeader from '../components/Header/DashboardHeader'
+import MobileMenu from '../components/common/mobile-menu/MobileMenu'
+
+function DashboardLayout ({ children }) {
+  return (
+    <>
+      {/* Main Header Nav */}
+      <DashboardHeader />
+      {/* End Main Header Nav */}
+
+      {/* Mobile Nav  */}
+      <MobileMenu />
+      {/* End Mobile Nav  */}
+
+      <div className='dashboard_content_wrapper'>
+        <div className='dashboard dashboard_wrapper pr30 pr0-xl'>
+          <SidebarDashboard />
+          <div className='dashboard__main pl0-md'>
+            <div className='dashboard__content bgc-f7'>
+              {children}
+            </div>
+
+            <DashboardFooter />
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default DashboardLayout
