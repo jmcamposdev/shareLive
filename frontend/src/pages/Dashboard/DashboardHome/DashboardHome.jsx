8c8f0d132463fb5151649660a49e6bb6c56c4fa9
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-29 11:29:22 +0100

    Fix class attribute to className in TitleBoxDashboard and DashboardHome components

--- frontend/src/pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ frontend/src/pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -9,11 +9,11 @@ function DashboardHome () {
     <DashboardLayout>
       <TitleDashboard title='Howdy, Ali!' subtitle='We are glad to see you again!' />
       <TopStateBlock />
-      <div class='flex flex-wrap '>
-        <div class='w-full xl:w-2/3 pr-4 pl-4'>
+      <div className='flex flex-wrap '>
+        <div className='w-full xl:w-2/3 pr-4 pl-4'>
           <WeeklyRoomsChart />
         </div>
-        <div class='w-full xl:w-1/3 pr-4 pl-4'>
+        <div className='w-full xl:w-1/3 pr-4 pl-4'>
           <RecentActivities />
         </div>
       </div>
