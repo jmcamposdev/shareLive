9e2e31a294bfc0fe58ec7a3cda4a329f0c7ef4ed
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-03-31 21:51:58 +0200

    Started the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/WebPages/Home/Home'
 import AboutUs from './pages/WebPages/AboutUs/AboutUs'
+import Login from './pages/WebPages/Login/Login.jsx'
 import NotFound from './pages/WebPages/404/404'
 import './css/style.css'
 import './css/clarityCity.css'
@@ -15,6 +16,7 @@ function App () {
       <Route path='/' element={<Home />} />
       <Route path='about' element={<AboutUs />} />
       <Route path='rooms' element={<Rooms />} />
+      <Route path='login' element={<Login />} />
 
       {/* Routes for the Dashboard */}
       <Route path='dashboard' element={<DashboardHome />} />
