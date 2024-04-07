a27c8076c85ce08531d7d6f474d0a13b0c70c639
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-04-07 13:23:15 +0200

    Now if there is no phone, mobile, or socials networks these wont be displayed

--- frontend/src/pages/WebPages/UserSingle/UserSingle.jsx
+++ frontend/src/pages/WebPages/UserSingle/UserSingle.jsx
@@ -48,24 +48,44 @@ const UserSingle = ({ user }) => {
                       <a className='text fz15 pe-2 bdrr1' href='#'>
                         <i className='fas fa-star fz10 review-color2 pr10' />{reviewsMssg}
                       </a>
-                      <a className='text fz15 pe-2 ps-2 bdrr1' href='#'><i className='flaticon-call pe-1' />{user.phone}</a><a className='text fz15 ps-2' href='#'><i className='flaticon-smartphone pe-1' />{user.mobilePhone}</a></div>
+                      {user.phone ? (<a className='text fz15 pe-2 ps-2 bdrr1'><i className='flaticon-call pe-1' />{user.phone}</a>) : ''}
+                      {user.mobilePhone ? (<a className='text fz15 ps-2'><i className='flaticon-smartphone pe-1' />{user.mobilePhone}</a>) : ''}
+                    </div>
                     <div className='agent-social'>
-                      <a className='mr20' href={`https://${user.socials.facebook}`}>
-                        <i className='fab fa-facebook-f' />
-                      </a>
-                      <a className='mr20 group' href={`https://${user.socials.twitter}`}>
+                      {user.socials.facebook ? (
+                          <a className='mr20' href={`https://${user.socials.facebook}`}>
+                            <i className='fab fa-facebook-f' />
+                          </a>)
+                        :
+                          ''
+                      }
+                      {user.socials.instagram ? (
+                          <a className='mr20' href={`https://${user.socials.instagram}`}>
+                            <i className='fab fa-instagram' />
+                          </a>)
+                        :
+                          ''
+                      }
+                      {user.socials.linkedin ? (
+                          <a className='mr20' href={`https://${user.socials.linkedin}`}>
+                            <i className='fab fa-linkedin-in' />
+                          </a>)
+                        :
+                          ''
+                      }
+                      {user.socials.twitter ? (
+                        <a className='mr20 group' href={`https://${user.socials.twitter}`}>
                         <i className='fab fa-twitter text-transparent relative'>
                           <svg className='absolute top-[6px] left-0' width='14' height='13' viewBox='0 0 14 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
                             <path style={{ transition: 'all ease 0.4s' }} className='fill-black group-hover:fill-orangePrimary' d='M10.6945 0.163574H12.7717L8.23961 5.3629L13.59 12.438H9.38522L6.10574 8.1451L2.329 12.438H0.264374L5.13008 6.8736L0 0.163574H4.2929L7.26395 4.0851L10.6945 0.163574ZM9.96432 11.1917H11.1162L3.66974 1.34066H2.41083L9.96432 11.1917Z' />
                           </svg>
                         </i>
                       </a>
-                      <a className='mr20' href={`https://${user.socials.instagram}`}>
-                        <i className='fab fa-instagram' />
-                      </a>
-                      <a className='mr20' href={`https://${user.socials.facebook}`}>
-                        <i className='fab fa-linkedin-in' />
-                      </a>
+                      )
+                        :
+                        ''
+                      }
+                      
                     </div>
                   </div>
                 </div>
@@ -85,7 +105,7 @@ const UserSingle = ({ user }) => {
               <div className='flex flex-wrap '>
                 <div className='lg:w-full pr-4 pl-4'>
                   <div className='agent-single-details mt30 pb30 bdrb1'>
-                    <h6 className='fz17 mb30'>About Agents</h6>
+                    <h6 className='fz17 mb30'>About {user.firstName}</h6>
                     <p className='text'>{user.description}</p>
                     
                     {user.extraDescription.length > 0 ? (
