5a4b31e43a201d239a7d84daaf1acc098fd3579b
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-04-14 19:38:48 +0200

    Created a new component for contact form of the single pages

---
+++ frontend/src/components/common/contactFormSingle/ContactFormSingle.jsx
@@ -0,0 +1,28 @@
+const ContactFormSingle = ({ title }) => {
+  return (
+    <div className='agent-single-form home8-contact-form default-box-shadow1 bdrs12 bdr1 p30 mb30-md bgc-white relative dark:bg-midnight dark:border-slate-400/20  '>
+      <h4 className='form-title mb25 dark:text-white'>{title}</h4>
+      <form className='form-style1'>
+        <div className='flex flex-wrap '>
+          <div className='w-full'>
+            <div className='mb20'><input className=' form-control block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded' placeholder='Your Name' required='' type='text' /></div>
+          </div>
+          <div className='w-full'>
+            <div className='mb20'><input className='form-control block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded' placeholder='Phone' required='' type='text' /></div>
+          </div>
+          <div className='w-full'>
+            <div className='mb20'><input className='form-control block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded' placeholder='Email' required='' type='email' /></div>
+          </div>
+          <div className='w-full'>
+            <div className='mb10'><textarea cols='30' rows='4' placeholder='There are many variations of passages.' required='' /></div>
+          </div>
+          <div className='w-full'>
+            <div className='d-grid'><button type='submit' className='ud-btn btn-thm mb15'>Send Message<i className='fal fa-arrow-right-long' /></button><a className='ud-btn btn-white2' href='/contact'>Call<i className='fal fa-arrow-right-long' /></a></div>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ContactFormSingle
--- frontend/src/pages/WebPages/UserSingle/UserSingle.jsx
+++ frontend/src/pages/WebPages/UserSingle/UserSingle.jsx
@@ -9,6 +9,7 @@ import CircleOrange from '../../../assets/img/users/circleOrange.png'
 import Bounce1Dark from '../../../assets/img/users/bounceDark.png'
 import Bounce2Dark from '../../../assets/img/users/bounce2Dark.png'
 import CircleOrangeDark from '../../../assets/img/users/circleOrangeDark.png'
+import ContactFormSingle from '../../../components/common/contactFormSingle/ContactFormSingle'
 
 const UserSingle = ({ user, rooms }) => {
   // Getting the reviews made of the user
@@ -272,28 +273,7 @@ const UserSingle = ({ user, rooms }) => {
               </div>
             </div>
             <div className='lg:w-1/3 pr-4 pl-4'>
-              <div className='agent-single-form home8-contact-form default-box-shadow1 bdrs12 bdr1 p30 mb30-md bgc-white relative dark:bg-midnight dark:border-slate-400/20  '>
-                <h4 className='form-title mb25 dark:text-white'>Contact Form</h4>
-                <form className='form-style1'>
-                  <div className='flex flex-wrap '>
-                    <div className='w-full'>
-                      <div className='mb20'><input className=' form-control block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded' placeholder='Your Name' required='' type='text' /></div>
-                    </div>
-                    <div className='w-full'>
-                      <div className='mb20'><input className='form-control block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded' placeholder='Phone' required='' type='text' /></div>
-                    </div>
-                    <div className='w-full'>
-                      <div className='mb20'><input className='form-control block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded' placeholder='Email' required='' type='email' /></div>
-                    </div>
-                    <div className='w-full'>
-                      <div className='mb10'><textarea cols='30' rows='4' placeholder='There are many variations of passages.' required='' /></div>
-                    </div>
-                    <div className='w-full'>
-                      <div className='d-grid'><button type='submit' className='ud-btn btn-thm mb15'>Send Message<i className='fal fa-arrow-right-long' /></button><a className='ud-btn btn-white2' href='/contact'>Call<i className='fal fa-arrow-right-long' /></a></div>
-                    </div>
-                  </div>
-                </form>
-              </div>
+              <ContactFormSingle title='Contact Me' />
               <div className='agen-personal-info relative bgc-white default-box-shadow1 bdrs12 p30 mt30 dark:bg-midnight'>
                 <div className='widget-wrapper mb-0'>
                   <h6 className='title fz17 mb35 dark:text-white'>More Information</h6>
