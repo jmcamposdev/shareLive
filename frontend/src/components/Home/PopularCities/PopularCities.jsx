fff26be0ac8931a636dd45eaa502ca75c6774aed
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-04-15 20:13:16 +0200

    Popular cities now has dark mode

--- frontend/src/components/Home/PopularCities/PopularCities.jsx
+++ frontend/src/components/Home/PopularCities/PopularCities.jsx
@@ -4,20 +4,20 @@ import CityCard from './CityCard/CityCard'
 
 const PopularCities = () => {
   return (
-    <section className='pt90 pb90 bgc-white'>
+    <section className='pt90 pb90 bg-white dark:!bg-lightmidnight'>
       <div className='container mx-auto sm:px-4'>
         <div className='flex flex-wrap justify-between  items-center' data-aos='fade-up'>
           <div className='lg:w-3/4 w-auto pr-4 pl-4'>
             <div className='main-title2'>
-              <h2 className='title'>Popular Cities</h2>
-              <p className='paragraph'>
+              <h2 className='title dark:text-white'>Popular Cities</h2>
+              <p className='paragraph dark:text-white'>
                 See our most rented cities
               </p>
             </div>
           </div>
           <div className='lg:w-1/4 w-auto pr-4 pl-4'>
             <div className='text-end sm:text-start mb-3'>
-              <Link className='ud-btn2' href='/grid-full-3-col'>
+              <Link className='ud-btn2 dark:text-white' href='/grid-full-3-col'>
                 See All Cities
                 <i className='fal fa-arrow-right-long' />
               </Link>
