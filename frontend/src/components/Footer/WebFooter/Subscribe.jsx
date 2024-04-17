87f521e25b93ad0d9f5070d9b077072b4c493a23
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-04-17 19:14:07 +0200

    The header now has dark mode

--- frontend/src/components/Footer/WebFooter/Subscribe.jsx
+++ frontend/src/components/Footer/WebFooter/Subscribe.jsx
@@ -3,7 +3,7 @@ const Subscribe = () => {
     <div className='mailchimp-widget mb-4 lg:mb-12'>
       <h6 className='title text-white mb20'>Keep Yourself Up to Date</h6>
       <div className='mailchimp-style1'>
-        <input type='email' className='form-control mx-2 placeholder:text-midnight bg-white !rounded-2xl' placeholder='Your Email' />
+        <input type='email' className='!text-midnight form-control mx-2 placeholder:text-midnight bg-white !rounded-2xl' placeholder='Your Email' />
         <button style={{ top: 'calc(41px + 50% - (133.3px / 2)' }} type='submit' className='ud-btn btn-thm !bg-orangePrimary'>Subscribe</button>
       </div>
     </div>
