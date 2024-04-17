6f426ec09e83b7b6b2f4faac9ece6d7e52782157
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-04-17 20:22:11 +0200

    chore: Format validation errors in auth.validators middleware

--- backend/src/middlewares/auth.validators.js
+++ backend/src/middlewares/auth.validators.js
@@ -7,7 +7,10 @@ const validate = (validations) => {
 
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
+      const formattedErrors = errors.array().map(error => ({
+        message: error.msg
+      }))
+      return res.status(400).json({ errors: formattedErrors })
     }
     next()
   }
