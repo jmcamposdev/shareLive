a6aab67354383db67b0fb68572570415156d6635
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-04-21 00:31:47 +0200

    Dashboard sidebar now has dark mode

--- frontend/src/components/Header/DashboardHeader.jsx
+++ frontend/src/components/Header/DashboardHeader.jsx
@@ -3,6 +3,7 @@ import MainMenu from '../common/MainMenu'
 import LogoWhite from '../../assets/logos/logo-white.png'
 import LogoDark from '../../assets/logos/logo-dark.png'
 import userImage from '../../assets/img/dashboard/header/user-default.png'
+import DarkModeSwitcher from '../common/darkModeSwitcher/DarkModeSwitcher'
 
 const DashboardHeader = () => {
   const menuItems = [
@@ -40,7 +41,7 @@ const DashboardHeader = () => {
   ]
 
   return (
-    <header className='header-nav nav-homepage-style light-header !fixed menu-home4 main-menu'>
+    <header className='header-nav nav-homepage-style light-header !fixed menu-home4 main-menu dark:bg-midnight'>
       <nav className='posr'>
         <div className='max-w-full mx-auto sm:px-4 pr30 pr15-xs pl30 posr menu_bdrt1'>
           <div className='flex flex-wrap  items-center justify-between'>
@@ -51,6 +52,12 @@ const DashboardHeader = () => {
                     <img
                       src={LogoDark}
                       alt='Header Logo'
+                      className='dark:hidden'
+                    />
+                    <img
+                      src={LogoWhite}
+                      alt='Header Logo'
+                      className='hidden dark:block'
                     />
                   </Link>
                 </div>
@@ -68,16 +75,22 @@ const DashboardHeader = () => {
             <div className='col-lg-auto'>
               <div className='text-center text-lg-end header_right_widgets'>
                 <ul className='mb0 flex justify-center sm:justify-end p-0'>
+                  {/* Dark Mode Switcher */}
+                  <li className='hidden sm:flex justify-center items-center w-[84px]'>
+                    <DarkModeSwitcher />
+                  </li>
+                  {/* End Dark Mode Switcher */}
+
                   <li className='hidden sm:block'>
                     <Link className='text-center mr15 !flex justify-center items-center' href='/login'>
-                      <span className='flaticon-email !flex' />
+                      <span className='flaticon-email !flex dark:text-white' />
                     </Link>
                   </li>
                   {/* End email box */}
 
                   <li className='hidden sm:block'>
-                    <a className='text-center mr20 notif !flex justify-center items-center' href='#'>
-                      <span className='flaticon-bell !flex' />
+                    <a className='text-center mr20 notif !flex justify-center items-center dark:bg-lightmidnight' href='#'>
+                      <span className='flaticon-bell !flex dark:text-white' />
                     </a>
                   </li>
                   {/* End notification icon */}
@@ -120,6 +133,7 @@ const DashboardHeader = () => {
                     </div>
                   </li>
                   {/* End avatar dropdown */}
+
                 </ul>
               </div>
             </div>
--- frontend/src/components/Rooms/TopFilterBar/TopFilterBar.jsx
+++ frontend/src/components/Rooms/TopFilterBar/TopFilterBar.jsx
@@ -11,6 +11,86 @@ const TopFilterBar = () => {
   const { gridView, orderBy, loading } = useContext(RoomContext)
   const { isGridView, onListClick, onGridClick } = gridView
 
+  const handleOrderSelect = (e) => {
+    switch (e.target.value) {
+      case ORDER_BY_NEWEST:
+        orderBy(ORDER_BY_NEWEST)
+        break
+      case ORDER_BY_LOW_PRICE:
+        orderBy(ORDER_BY_LOW_PRICE)
+        break
+      case ORDER_BY_HIGH_PRICE:
+        orderBy(ORDER_BY_HIGH_PRICE)
+        break
+      default:
+        break
+    }
+  }
+  return (
+    <>
+      <div className='sm:w-1/2 pr-4 pl-4'>
+        <div className='text-center text-sm-start'>
+          {
+            loading
+              ? (
+                <LoadingSkeleton className='w-40 h-4' />
+                )
+              : (
+                <p className='pagination_page_count mb-0 text-left relative z-10 dark:text-white'>Showing {currentPage} of {totalPages} pages</p>
+                )
+          }
+        </div>
+      </div>
+      {/* End .col-sm-6 */}
+
+      <div className='sm:w-1/2 pr-4 pl-4'>
+        <div className='page_control_shorting flex items-center justify-center sm:justify-end'>
+          <div className='pcs_dropdown pr10 flex items-center'>
+            <span className='min-w-[60px]'>Sort by</span>
+            <select
+              className='form-select dark:text-white'
+              onChange={handleOrderSelect}
+            >
+              <option className='' value={ORDER_BY_NEWEST}>Newest</option>
+              <option className='' value={ORDER_BY_LOW_PRICE}>Price Low</option>
+              <option className='' value={ORDER_BY_HIGH_PRICE}>Price High</option>
+            </select>
+          </div>
+          <button
+            className={`pl15 pr15 bdrl1 bdrr1 d-none d-md-block cursor dark:text-white ${isGridView ? 'menuActive' : '#'}`}
+            onClick={onGridClick}
+          >
+            Grid
+          </button>
+          <button
+            className={`pl15 d-none d-md-block  cursor dark:text-white ${!isGridView ? 'menuActive' : '#'}`}
+            onClick={onListClick}
+          >
+            List
+          </button>
+        </div>
+      </div>
+      {/* End .col-sm-6 */}
+    </>
+  )
+}
+
+export default TopFilterBar
+
+/*
+import React, { useContext } from 'react'
+import { RoomContext } from '../../../context/roomContext'
+import { ORDER_BY_HIGH_PRICE, ORDER_BY_LOW_PRICE, ORDER_BY_NEWEST } from '../../../constants/rooms.constants'
+import { PaginationContext } from '../../../context/PaginationContext'
+import LoadingSkeleton from '../../common/Loading/LoadingSkeleton'
+
+const TopFilterBar = () => {
+  // Get the paginacion context currentPage, totalPages
+  const { currentPage, totalPages } = useContext(PaginationContext)
+  // Get the rooms from the context
+  const { gridView, orderBy, loading } = useContext(RoomContext)
+  const { isGridView, onListClick, onGridClick } = gridView
+
   const handleOrderSelect = (value) => {
     switch (value) {
       case ORDER_BY_NEWEST:
@@ -73,7 +153,6 @@ const TopFilterBar = () => {
           }
         </div>
       </div>
-      {/* End .col-sm-6 */}
 
       <div className='sm:w-1/2 pr-4 pl-4'>
         <div className='page_control_shorting flex items-center justify-center sm:justify-end'>
@@ -107,9 +186,10 @@ const TopFilterBar = () => {
           </button>
         </div>
       </div>
-      {/* End .col-sm-6 */}
     </>
   )
 }
 
 export default TopFilterBar
+
+*/
