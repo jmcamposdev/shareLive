a7681ac69f8b65877611fd9eac3b64c004bdddb9
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-04-22 18:45:37 +0200

    Refactor ScrollToTop component to improve page scrolling behavior

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 import { LocationProvider } from './context/LocationContext.jsx'
+import ScrollToTop from './features/ScrollToTop.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <LocationProvider>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </LocationProvider>
