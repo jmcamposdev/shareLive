c22236d91e6fc5cc2f93fb886f820ac9b2bdf777
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-05-02 20:55:27 +0200

    refactor: Add delete and edit functionality to SearchRoomsTable component

--- frontend/src/components/Dashboard/SearchRooms/SearchRoomsTable/SearchRoomsTable.jsx
+++ frontend/src/components/Dashboard/SearchRooms/SearchRoomsTable/SearchRoomsTable.jsx
@@ -3,19 +3,40 @@ import roomsColumns from './searchRoomsColumns.jsx'
 import Table from '../../../common/Table/Table'
 import { useState } from 'react'
 import BoxDashboard from '../../../common/Dashboard/BoxDashboard/BoxDashboard'
+import RoomService from '../../../../services/roomService.js'
+import useAlertToast from '../../../../hooks/useToast.js'
 
-const SearchRoomsTable = ({ rooms }) => {
+const SearchRoomsTable = ({ rooms, loading, title, subTitle }) => {
+  const { toast } = useAlertToast()
   const [filter, setFilter] = useState('')
+
+  const onDelete = (roomId) => {
+    RoomService.deleteRoom(roomId)
+      .then(() => {
+        toast.showSuccess('Room deleted successfully')
+        console.log('Room deleted successfully')
+      })
+      .catch((error) => {
+        toast.showError(error.message)
+        console.error('Error deleting room:', error.message)
+      }
+      )
+  }
+
+  const onEdit = (roomId) => {
+    console.log('Edit room:', roomId)
+  }
+
   return (
     <>
-      <div className='flex justify-between items-center mb30'>
-        <TitleDashboard title='Search Rooms' subtitle='Search all for rooms here!' />
-        <div className='flex gap-4'>
+      <div className='flex justify-between items-center'>
+        <TitleDashboard title={title} subtitle={subTitle} />
+        <div className='flex gap-4 pb40'>
           <div className='item1 mb15-sm'>
             <div className='search_area'>
               <input
                 type='text'
-                className='form-control bdrs12 dark:bg-midnight dark:!border-borderColor/20 placeholder:text-borderColor/40 dark:text-white dark:outline-white'
+                className='form-control bdrs12 dark:bg-midnight dark:!border-borderColor/20 placeholder:text-[#181A20]/50 dark:text-white dark:outline-white'
                 placeholder='Search'
                 required=''
                 value={filter}
@@ -32,7 +53,15 @@ const SearchRoomsTable = ({ rooms }) => {
       {/* End Title | Search | Add Room */}
 
       <BoxDashboard>
-        <Table data={rooms} columns={roomsColumns} filterValue={filter} onFilter={setFilter} />
+        <Table
+          data={rooms}
+          loading={loading}
+          columns={roomsColumns}
+          filterValue={filter}
+          onFilter={setFilter}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
       </BoxDashboard>
 
     </>
