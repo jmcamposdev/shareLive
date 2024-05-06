68246c2d4128c9bc52c41073355fdef0b64fa253
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-05-06 19:28:02 +0200

    feat: Add toggleFavoriteRoom and getFavouriteRooms API endpoints

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -70,4 +70,50 @@ const getUserRooms = async (req, res) => {
   }
 }
 
-export { getUsers, getUser, updateUser, getUserRooms }
+const getFavouriteRooms = async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findById(id)
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found'
+      })
+    }
+    const rooms = await Room.find({ _id: { $in: user.favouriteRoomsIds } }).populate('user')
+    res.json(rooms)
+  } catch (error) {
+    res.status(500).json({
+      message: 'An error occurred while getting user rooms',
+      error
+    })
+  }
+}
+
+const toggleFavoriteRoom = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { roomId } = req.body
+    const user = await User.findById(id)
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found'
+      })
+    }
+
+    const isFavorite = user.favouriteRoomsIds.includes(roomId)
+    if (isFavorite) {
+      user.favouriteRoomsIds = user.favouriteRoomsIds.filter((id) => id !== roomId)
+    } else {
+      user.favouriteRoomsIds.push(roomId)
+    }
+    await user.save()
+    res.json(user)
+  } catch (error) {
+    res.status(500).json({
+      message: 'An error occurred while adding a favorite room',
+      error
+    })
+  }
+}
+
+export { getUsers, getUser, updateUser, getUserRooms, toggleFavoriteRoom, getFavouriteRooms }
