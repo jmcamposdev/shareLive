537e2e05ca9754ed7679c473c1f80a444efa9d52
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-05-11 10:03:05 +0200

    feat: Remove review from assigned user when deleted

--- backend/src/models/Review.js
+++ backend/src/models/Review.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose'
+import User from './User.js'
 
 const reviewSchema = new Schema({
   ownerId: { type: String, required: true }, // Almacenar el id del usuario
@@ -10,6 +11,21 @@ const reviewSchema = new Schema({
   helpful: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
   notHelpful: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
   createdAt: { type: Date, default: Date.now }
+})
+
+/**
+ * Trigger when a review is deleted
+ * This hook will remove the review from the assigned user
+ */
+reviewSchema.pre('findOneAndDelete', async function (next) {
+  // `this` se refiere a la consulta que se está ejecutando, así que puedes acceder a los datos de la review a eliminar
+  const review = await this.model.findOne(this.getFilter())
+  // Delete the review from the assigned user
+  const assignedUser = await User.findById(review.assignedId)
+  assignedUser.reviews = assignedUser.reviews.filter(reviewId => reviewId.toString() !== review._id.toString())
+  await assignedUser.save()
+
+  next()
 })
 
 const Review = mongoose.model('Review', reviewSchema)
