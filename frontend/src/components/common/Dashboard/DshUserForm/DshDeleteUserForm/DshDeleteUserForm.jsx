da1cde6795c64908d24db941e0aae1fce34ced2a
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-05-16 19:32:23 +0200

    refactor: Remove unused code in DshDeleteUserForm component
    This commit removes the unused import statement for Form and Formik in the DshDeleteUserForm component. The code was commented out and no longer needed, so it was removed to improve code cleanliness and maintainability.

--- frontend/src/components/common/Dashboard/DshUserForm/DshDeleteUserForm/DshDeleteUserForm.jsx
+++ frontend/src/components/common/Dashboard/DshUserForm/DshDeleteUserForm/DshDeleteUserForm.jsx
@@ -1,4 +1,3 @@
-import { Form, Formik } from 'formik'
 import BoxDashboard from '../../BoxDashboard/BoxDashboard'
 import UserService from '../../../../../services/UserService'
 import useAlertToast from '../../../../../hooks/useToast'
@@ -8,6 +7,7 @@ import { useDialogoConfirmarCambiarV2 } from '../../../../../hooks/useDialogoCon
 const DshDeleteUserForm = ({ user }) => {
   const { toast } = useAlertToast()
   const navigate = useNavigate()
+
   const onSubmit = async (values, actions) => {
     actions.setSubmitting(true)
     try {
@@ -21,35 +21,29 @@ const DshDeleteUserForm = ({ user }) => {
     }
   }
 
-  const { DialogoConfirmarCambiar, handleMostrarDialogo } = useDialogoConfirmarCambiarV2({
-    onCambiar: () => { console.log('cambio') }
+  const { DialogElement, handleOpen } = useDialogoConfirmarCambiarV2({
+    onSave: onSubmit,
+    title: 'Delete Account',
+    message: 'Are you sure you want to delete your account? This action cannot be undone.',
+    deleteElementName: 'Account'
   })
 
   return (
     <BoxDashboard>
       <h4 className='title fz17 mb30 dark:text-white'>Delete Account</h4>
-      <Formik
-        initialValues={{}}
-        onSubmit={onSubmit}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <div className='flex items-center justify-between'>
-              <p className='fz14 dark:text-white m-0'>
-                Are you sure you want to delete your account? This action cannot be undone.
-              </p>
-              <div className='text-end'>
-                {/* <FormikSubmitBtn label='Delete Account' isSubmitting={isSubmitting} isDanger /> */}
-                <a type='button' className='ud-btn bg-red-500 before:bg-[#c93939] hover:bg-red-500' onClick={() => handleMostrarDialogo(false)}>
-                  Delete Account
-                  <i className='fal fa-arrow-right-long' />
-                </a>
-              </div>
-            </div>
-          </Form>
-        )}
-      </Formik>
-      {DialogoConfirmarCambiar}
+      <div className='flex items-center justify-between'>
+        <p className='fz14 dark:text-white m-0'>
+          Are you sure you want to delete your account? This action cannot be undone.
+        </p>
+        <div className='text-end'>
+          {/* <FormikSubmitBtn label='Delete Account' isSubmitting={isSubmitting} isDanger /> */}
+          <a type='button' className='ud-btn bg-red-500 before:bg-[#c93939] hover:bg-red-500 text-white' onClick={() => handleOpen()}>
+            Delete Account
+            <i className='fal fa-arrow-right-long' />
+          </a>
+        </div>
+      </div>
+      {DialogElement}
     </BoxDashboard>
   )
 }
--- frontend/src/components/common/Dialogs/DialogConfirmDel.jsx
+++ frontend/src/components/common/Dialogs/DialogConfirmDel.jsx
@@ -1,66 +1,63 @@
-import PropTypes from 'prop-types'
+import { Form, Formik } from 'formik'
+import FormikSubmitBtn from '../Formik/Buttons/FormikSubmitBtn'
 
-const DialogConfirmDel = ({ titulo, texto1, texto2, accionPrincipalLabel, accionDescartarLabel, onAccionPrincipal, onAccionDescartar, onHide, visible, children, size }) => {
+const DialogConfirmDel = ({ open, onCancel, onSave, title, message, deleteElementName }) => {
   return (
-    <div className={`fixed z-10 inset-0 overflow-y-auto ${visible ? 'block' : 'hidden'}`}>
-      <div className='flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
-        <div className='fixed inset-0 transition-opacity'>
-          <div className='absolute inset-0 bg-gray-500 opacity-75' />
-        </div>
-
-        <span className='hidden sm:inline-block sm:align-middle sm:h-screen' />&#8203;
-
-        <div className='inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'>
-          <div className='absolute top-0 right-0 pt-2 pr-2'>
-            <button onClick={onHide} className='text-gray-500 hover:text-gray-700 focus:outline-none'>
-              <i className='fas fa-times text-base' />
-            </button>
-          </div>
-          <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
-            <div className='sm:flex sm:items-start'>
-              <div className='mt-3 text-center sm:mt-0 sm:text-left'>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>{titulo}</h3>
-                <div className='mt-2'>
-                  {texto1 && <p className='text-sm text-gray-500'>{texto1}</p>}
-                  {texto2 && <p className='text-sm text-gray-500'>{texto2}</p>}
-                  {children}
+    <div
+      className={`fixed left-0 top-0 z-50 flex h-full min-h-screen w-full items-center justify-center bg-black/90 px-4 py-5 ${open ? 'block' : 'hidden'}`}
+    >
+      <div className='max-w-[620px] max-w-142.5 rounded-lg bg-white px-8 py-12 text-center dark:bg-boxdark md:px-17.5 md:py-15'>
+        <Formik
+          initialValues={{}}
+          onSubmit={onSave}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <span className='mx-auto inline-block'>
+                <svg
+                  width={60}
+                  height={60}
+                  viewBox='0 0 60 60'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <rect opacity='0.1' width={60} height={60} rx={30} fill='#DC2626' />
+                  <path
+                    d='M30 27.2498V29.9998V27.2498ZM30 35.4999H30.0134H30ZM20.6914 41H39.3086C41.3778 41 42.6704 38.7078 41.6358 36.8749L32.3272 20.3747C31.2926 18.5418 28.7074 18.5418 27.6728 20.3747L18.3642 36.8749C17.3296 38.7078 18.6222 41 20.6914 41Z'
+                    stroke='#DC2626'
+                    strokeWidth='2.2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  />
+                </svg>
+              </span>
+              <h3 className='mt-5.5 pb-2 text-xl font-bold text-black dark:text-white sm:text-2xl'>
+                {title}
+              </h3>
+              <p className='mb-10 font-medium'>
+                {message}
+              </p>
+              <div className='mx-3 flex gap-y-4 pt-10'>
+                <div className='w-full px-3 '>
+                  <button
+                    type='button'
+                    className='block w-full rounded border border-stroke bg-gray p-3 text-center font-medium text-black transition hover:border-meta-1 hover:bg-meta-1 hover:text-white dark:border-strokedark dark:bg-meta-4 dark:text-white dark:hover:border-meta-1 dark:hover:bg-meta-1'
+                    onClick={onCancel}
+                  >
+                    Cancel
+                  </button>
+                </div>
+                <div className='w-full px-3'>
+                  <FormikSubmitBtn label={`Delete ${deleteElementName}`} isSubmitting={isSubmitting} isDanger />
                 </div>
               </div>
-            </div>
-          </div>
-          <div className='px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse gap-5'>
-            <button
-              onClick={onAccionPrincipal}
-              type='submit' className='ud-btn text-white dark:border-[#dddddd26] cursor-pointer bg-red-500 before:bg-[#c93939] hover:bg-red-500'
-            >{accionPrincipalLabel}<i className='fal fa-arrow-right-long' />
-            </button>
-            <button
-              onClick={onAccionDescartar}
-              className='ud-btn btn-white2 dark:text-white'
-              type='button'
-            >{accionDescartarLabel}<i class='fal fa-arrow-right-long' />
-            </button>
-
-          </div>
-        </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
 
   )
-}
-
-DialogConfirmDel.propTypes = {
-  titulo: PropTypes.string,
-  texto1: PropTypes.string,
-  texto2: PropTypes.string,
-  accionPrincipalLabel: PropTypes.string,
-  accionDescartarLabel: PropTypes.string,
-  onAccionPrincipal: PropTypes.func,
-  onAccionDescartar: PropTypes.func,
-  onHide: PropTypes.func,
-  visible: PropTypes.bool,
-  children: PropTypes.node,
-  size: PropTypes.string
 }
 
 export default DialogConfirmDel
