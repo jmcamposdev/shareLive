71c35c13d2b173a8dca6ce5f71571a3c761b282b
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-06-05 19:51:16 +0200

    refactor: Remove commented out code for Google and Apple login buttons
    This commit removes the commented out code for the Google and Apple login buttons in the Login.jsx file. The code was no longer needed and removing it improves the clarity and maintainability of the codebase.

--- frontend/src/pages/WebPages/Login/Login.jsx
+++ frontend/src/pages/WebPages/Login/Login.jsx
@@ -124,7 +124,7 @@ const Login = () => {
                       <hr className='absolute top-[45%] w-full dark:text-white' />
                       <span className='hr_top_text dark:text-white dark:bg-midnight'>OR</span>
                     </div>
-                    <div className='d-grid mb10'>
+                    {/* <div className='d-grid mb10'>
                       <button className='ud-btn btn-white' type='button'>
                         <i className='fab fa-google' /> Continue Google
                       </button>
@@ -138,7 +138,7 @@ const Login = () => {
                       <button className='ud-btn btn-apple bg-black' type='button'>
                         <i className='fab fa-apple' /> Continue Apple
                       </button>
-                    </div>
+                    </div> */}
                     <p className='dark-color text-center mb0 mt10 dark:text-white'>
                       Not signed up? <Link className='dark-color fw600 dark:text-white' to='/register'>Create an account</Link>
                     </p>
