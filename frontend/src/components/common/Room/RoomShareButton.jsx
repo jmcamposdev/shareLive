9e18a5bea8da6ec818a59ba1554b3487d892a6ad
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-06-08 13:34:36 +0200

    chore: Update RoomShareButton tooltip text color to white

--- frontend/src/components/common/Room/RoomShareButton.jsx
+++ frontend/src/components/common/Room/RoomShareButton.jsx
@@ -59,10 +59,10 @@ const RoomShareButton = ({ room }) => {
         <span className='flaticon-share-1 dark:text-white' />
       </a>
       {showTooltip && (
-        <div className='absolute top-full right-0 mt-2 w-40 p-2 bg-white dark:bg-midnight border border-gray-200 dark:border-gray-700 rounded shadow-lg z-40'>
+        <div className='absolute top-full right-0 mt-2 w-40 p-2 bg-white dark:bg-midnight border border-gray-200 dark:border-gray-700 rounded shadow-lg z-40 dark:text-white'>
           <button onClick={() => handleShare('twitter')} className='flex gap-1 items-center w-full text-left px-2 py-1 hover:bg-gray-100 dark:hover:bg-gray-800'>
             <svg className='inline' width='14' height='14' viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
-              <path className='transition-all duration-500 ease group-hover:fill-white fill-[#181A20] ' d='M10.6945 0.163574H12.7717L8.23961 5.3629L13.59 12.438H9.38522L6.10574 8.1451L2.329 12.438H0.264374L5.13008 6.8736L0 0.163574H4.2929L7.26395 4.0851L10.6945 0.163574ZM9.96432 11.1917H11.1162L3.66974 1.34066H2.41083L9.96432 11.1917Z' />
+              <path className='transition-all duration-500 ease group-hover:fill-white fill-[#181A20] dark:fill-white ' d='M10.6945 0.163574H12.7717L8.23961 5.3629L13.59 12.438H9.38522L6.10574 8.1451L2.329 12.438H0.264374L5.13008 6.8736L0 0.163574H4.2929L7.26395 4.0851L10.6945 0.163574ZM9.96432 11.1917H11.1162L3.66974 1.34066H2.41083L9.96432 11.1917Z' />
             </svg>
             Twitter
           </button>
